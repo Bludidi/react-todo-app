6931816d554f4eca8d6c77d52d43674d87e8151c
Bludidi <bludidi5@gmail.com>
2022-11-15 06:58:37 +0200

    add checkbox in the TodoItem compnent

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,4 +1,4 @@
-/* eslint-disable */ 
+/* eslint-disable */
 
 import React from 'react';
 import TodoItem from "./TodoItem";
@@ -7,10 +7,14 @@ class TodoList extends React.Component {
   render() {
     return (
       <ul>
-      {this.props.todos.map(todo => (
-       <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
+        {this.props.todos.map(todo => (
+          <TodoItem 
+            key={todo.id} 
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+          />
+        ))}
+      </ul>
     )
   }
 };
